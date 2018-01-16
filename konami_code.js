f0ff94b3cd5783f53a73c482f9628345830d8251
Cara Kane <cara.kane@gmail.com>
2018-01-16 19:50:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(event) {
-    console.log(event.which)
+    console.log(event.detail)
   })
 
 }
